0200c7774cccca2dbf77bce9d4cbb680251b30c1
LorenzoComparini <lorenzo.comparini01@gmail.com>
2021-11-03 19:44:58 +0100

    Added migrations to db

---
+++ backend_cinemissimo/database/migrations/2021_11_03_183159_create_projections_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projections', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('date');
+            $table->boolean('3d');
+            $table->unsignedBigInteger('movie_id');
+            $table->foreign('movie_id')->references('id')->on('movies');
+            $table->unsignedBigInteger('room_id');
+            $table->foreign('room_id')->references('id')->on('rooms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projections');
+    }
+}
---
+++ backend_cinemissimo/database/migrations/2021_11_03_184040_create_reservations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->text('mail');
+            $table->integer('row');
+            $table->integer('col');
+            $table->unsignedBigInteger('projection_id');
+            $table->foreign('projection_id')->references('id')->on('projections');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
