18678f21717b696806195bb35dcd19849f1147cc
LorenzoComparini <lorenzo.comparini01@gmail.com>
2021-11-03 12:57:08 +0100

    Added Home component in frontend

--- frontend_cinemissimo/src/main.js
+++ frontend_cinemissimo/src/main.js
@@ -1,10 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+import Home from './components/Home'
+
+Vue.use(VueRouter)
 
 import "./style.css"
-
 Vue.config.productionTip = false
+
+const routes = [
+  {
+    path: '/home',
+    name: 'home',
+    component: Home 
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
