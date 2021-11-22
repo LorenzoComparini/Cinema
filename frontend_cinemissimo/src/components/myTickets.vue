a2464d1bf1b770811e74369fea9b9475bbb79bd3
Andrea G <andrea.grendene1@gmail.com>
2021-11-22 22:07:12 +0100

    Aggiunta possibilità di aggiungere 'Google Material Icons'

--- frontend_cinemissimo/src/components/myTickets.vue
+++ frontend_cinemissimo/src/components/myTickets.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="flex flex-row h-full w-full pt-10">
-      <div class="flex flex-col w-1/3"></div>
-      <div class="flex flex-col w-1/3">
+      <div class="flex flex-col w-1/6"></div>
+      <div class="flex flex-col w-4/6">
           <div v-for="reservation in reservations" :key="reservation.id" class="flex flex-row max-w-screen-md w-full bg-white rounded-lg p-5 mb-6 m-auto">
             <div class="flex flex-col">
-                <img :src="reservation.projection.movie.img" class="h-48 min-w-max mr-7" alt="">
+                <img :src="reservation.projection.movie.img" class= "mr-7" alt="">
             </div>
             <div class="flex flex-col h-full w-full pl-8">
                 <div class="flex justify-between w-full">
                     <p class="text-xl font-semibold">{{ reservation.projection.movie.title }}</p>
-
-                    <img
-                    class="cursor-pointer hover:bg-giallo rounded-full p-2"
-                    src="https://img.icons8.com/material-outlined/24/000000/trash--v1.png"
-                    @click="deleteReservation()"
-                    />
+                    <button
+                        class="cursor-pointer hover:bg-giallo rounded-full p-3"
+                        @click="deleteReservation()"
+                    > 
+                        <span class="material-icons pt-1">
+                            delete
+                        </span>
+                    </button>
                 </div>
                 <p class="text-lg italic">{{ reservation.name }} {{ reservation.surname }}</p>
 
@@ -28,7 +30,7 @@
             </div>
           </div>
       </div>
-      <div class="flex flex-col w-1/3"></div>
+      <div class="flex flex-col w-1/6"></div>
 
   </div>
 </template>
