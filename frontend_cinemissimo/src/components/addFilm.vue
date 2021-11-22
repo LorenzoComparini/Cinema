00f1ee7c9f8542e237dabd69c8e46bcda61f0063
Andrea G <andrea.grendene1@gmail.com>
2021-11-22 11:59:00 +0100

    Disabilitato bottone per aggiungere il film in caso di mancanza di dati con modifica anche visuale

--- frontend_cinemissimo/src/components/addFilm.vue
+++ frontend_cinemissimo/src/components/addFilm.vue
@@ -13,7 +13,29 @@
             <input class="rounded px-3 w-full py-1 outline-none" type="text" id="image" placeholder="Url immagine" v-model="movie.img">
 
             <div class="w-full flex flex-row justify-end mt-7">
-                <button class="text-blu-dark bg-giallo rounded-lg py-1 px-4" @click="addFilm()">
+                <button class="rounded-lg py-1 px-4" 
+                    @click="addFilm()"
+                    :disabled="this.movie.title == '' 
+                           || this.movie.year == null
+                           || this.movie.duration == null
+                           || this.movie.description == ''
+                           || this.movie.img == ''
+                              "
+                    :class="{
+                        'cursor-not-allowed bg-gray-600 text-white': 
+                           this.movie.title == '' 
+                           || this.movie.year == null
+                           || this.movie.duration == null
+                           || this.movie.description == ''
+                           || this.movie.img == '',
+                        'cursor-pointer  bg-giallo text-blu-dark': 
+                           this.movie.title != '' 
+                           && this.movie.year != null
+                           && this.movie.duration != null
+                           && this.movie.description != ''
+                           && this.movie.img != ''
+                        }"
+                >
                     Aggiungi film
                 </button>
             </div>
