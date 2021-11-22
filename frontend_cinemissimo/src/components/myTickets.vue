f51dcfbf61f13a59cda787fd1a0d83e11c8c3dfb
Andrea G <andrea.grendene1@gmail.com>
2021-11-22 09:50:58 +0100

    Ora si vedono in rosso i posti già riservati

--- frontend_cinemissimo/src/components/myTickets.vue
+++ frontend_cinemissimo/src/components/myTickets.vue
@@ -2,7 +2,11 @@
   <div class="flex flex-row h-full w-full p-20">
       <div class="flex flex-col">
           <div v-for="reservation in reservations" :key="reservation.id" class="flex flex-row bg-white rounded-lg py-7 px-10 mb-6">
-              <p>{{ reservation }}</p>
+              <div class="grid flex-column">
+                  <p>A nome di: <span class="font-semibold">{{ reservation.name }}</span></p>
+                  <p>Mail: <span class="font-semibold">{{ reservation.mail }}</span></p>
+                  <p>Id: <span class="font-semibold">{{ reservation.projection_id }} </span></p>
+              </div>
           </div>
       </div>
   </div>
